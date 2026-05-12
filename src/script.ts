a327a7865e67ecb118b7a3a66e54a51845195bdb
Fagner Ferreira <fagnerrc12@gmail.com>
2026-05-12 17:49:18 -0300

    animate clouds

--- src/script.ts
+++ src/script.ts
@@ -1,4 +1,37 @@
+const stars = document.querySelector(".stars") as HTMLElement;
+
 let selected: HTMLElement | null = null;
+
+const cloudContainer = document.querySelector("#clouds") as HTMLElement;
+
+const cloudData = [
+    {
+        x: 0.42,
+        y: 0.3,
+        width: 0.26,
+    },
+    { x: 0.3, y: 0.55, width: 0.17 },
+    { x: 0.15, y: 0.38, width: 0.14 },
+    { x: 0.24, y: 0.2, width: 0.13 },
+    { x: 0.58, y: 0.45, width: 0.24 },
+    { x: 0.7, y: 0.2, width: 0.13 },
+    { x: 0.75, y: 0.5, width: 0.15 },
+    { x: 0.8, y: 0.2, width: 0.11 },
+    { x: 0.89, y: 0.2, width: 0.19 },
+];
+
+for (let i = 0; i < cloudData.length; i++) {
+    const cloud = document.createElement("img");
+    cloud.src = `./assets/c${i + 1}.webp`;
+    cloud.classList.add("cloud");
+
+    cloud.style.left = `${stars.offsetLeft + cloudData[i]!.x * stars.clientWidth}px`;
+    cloud.style.top = `${stars.offsetTop + cloudData[i]!.y * stars.clientHeight * 0.35}px`;
+    cloud.style.width = `${stars.clientWidth * cloudData[i]!.width}px`;
+
+    console.log(stars.clientLeft);
+    cloudContainer.appendChild(cloud);
+}
 
 type Card = {
     style: CSSStyleDeclaration;
@@ -83,7 +116,6 @@ for (let j = 0; j < carrouselsData.length; j++) {
 
     requestAnimationFrame(animate);
 }
-const stars = document.querySelector(".stars") as HTMLElement;
 const nav = document.querySelector("nav") as HTMLElement;
 const header = document.querySelector("header") as HTMLElement;
 document.addEventListener("scroll", (e) => {
