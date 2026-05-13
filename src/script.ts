c095d9438ac6718cfafc6ce3466dd9768de49959
Fagner Ferreira <fagnerrc12@gmail.com>
2026-05-12 22:46:00 -0300

    move svg defs to file

--- src/script.ts
+++ src/script.ts
@@ -203,78 +203,84 @@ document.addEventListener("scroll", (e) => {
     nav.style.borderBottom = `1px solid hsl(0, 0%, 0%, ${value * 0.2})`;
 });
 
-import("animejs").then((animejs) => {
-    const { animate, svg, createTimeline } = animejs;
-    const links = document.querySelectorAll(".link");
-    for (const link of links) {
-        const path = link.querySelector("svg>path") as SVGElement;
-        const className = path.classList.values().next().value;
+fetch("./assets/defs.svg")
+    .then((response) => response.text())
+    .then(async (text) => {
+        const parser = new DOMParser();
+        const svgDoc = parser.parseFromString(text, "image/svg+xml");
+        document.body.appendChild(svgDoc.firstElementChild!);
 
-        const tl = createTimeline({ loop: true, autoplay: false });
+        const { animate, svg, createTimeline } = await import("animejs");
+        const links = document.querySelectorAll(".link");
+        for (const link of links) {
+            const path = link.querySelector("svg>path") as SVGElement;
+            const className = path.classList.values().next().value;
 
-        tl.add(path, {
-            d: svg.morphTo("#collapsed"),
-            duration: 500,
-        });
-        tl.add(path, {
-            d: svg.morphTo("#collapsedh"),
-            duration: 1000,
-        });
-        tl.add(path, {
-            d: svg.morphTo(`#${className}`),
-            duration: 1000,
-        });
-        tl.add(path, {
-            d: svg.morphTo("#circle"),
-            duration: 500,
-        });
-        tl.add(path, {
-            d: svg.morphTo("#collapsed"),
-            duration: 500,
-        });
-        tl.add(path, {
-            d: svg.morphTo("#peacock"),
-            duration: 500,
-        });
-        tl.add(path, {
-            d: svg.morphTo(`#${className}`),
-            duration: 500,
-        });
-        tl.add(path, {
-            d: svg.morphTo("#flower"),
-            duration: 1000,
-        });
-        tl.add(path, {
-            d: svg.morphTo(`#${className}`),
-            duration: 500,
-        });
-        tl.add(path, {
-            d: svg.morphTo("#violin"),
-            duration: 1000,
-        });
-        tl.add(path, {
-            d: svg.morphTo("#circle"),
-            duration: 500,
-        });
-        tl.add(path, {
-            d: svg.morphTo(`#${className}`),
-            duration: 500,
-        });
+            const tl = createTimeline({ loop: true, autoplay: false });
 
-        const initial = document
-            .querySelector(`#${className}`)
-            ?.getAttribute("d");
-        if (initial) path.setAttribute("d", initial);
-
-        (link as HTMLElement).addEventListener("mouseenter", () => {
-            tl.restart();
-        });
-        link.addEventListener("mouseleave", () => {
-            tl.pause();
-            animate(path, {
+            tl.add(path, {
+                d: svg.morphTo("#collapsed"),
                 duration: 500,
-                d: svg.morphTo(`#${className}`),
             });
-        });
-    }
-});
+            tl.add(path, {
+                d: svg.morphTo("#collapsedh"),
+                duration: 1000,
+            });
+            tl.add(path, {
+                d: svg.morphTo(`#${className}`),
+                duration: 1000,
+            });
+            tl.add(path, {
+                d: svg.morphTo("#circle"),
+                duration: 500,
+            });
+            tl.add(path, {
+                d: svg.morphTo("#collapsed"),
+                duration: 500,
+            });
+            tl.add(path, {
+                d: svg.morphTo("#peacock"),
+                duration: 500,
+            });
+            tl.add(path, {
+                d: svg.morphTo(`#${className}`),
+                duration: 500,
+            });
+            tl.add(path, {
+                d: svg.morphTo("#flower"),
+                duration: 1000,
+            });
+            tl.add(path, {
+                d: svg.morphTo(`#${className}`),
+                duration: 500,
+            });
+            tl.add(path, {
+                d: svg.morphTo("#violin"),
+                duration: 1000,
+            });
+            tl.add(path, {
+                d: svg.morphTo("#circle"),
+                duration: 500,
+            });
+            tl.add(path, {
+                d: svg.morphTo(`#${className}`),
+                duration: 500,
+            });
+
+            const initial = document
+                .querySelector(`#${className}`)
+                ?.getAttribute("d");
+            if (initial) path.setAttribute("d", initial);
+
+            (link as HTMLElement).addEventListener("mouseenter", () => {
+                tl.restart();
+            });
+            link.addEventListener("mouseleave", () => {
+                tl.pause();
+                animate(path, {
+                    duration: 500,
+                    d: svg.morphTo(`#${className}`),
+                });
+            });
+        }
+    });
