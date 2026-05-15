6f4412e835fbec08178c4e107f905510927108f1
Fagner Ferreira <fagnerrc12@gmail.com>
2026-05-15 17:02:21 -0300

    include element nodes in letters animation

--- src/scripts/letters.ts
+++ src/scripts/letters.ts
@@ -1,7 +1,7 @@
 import { animateCalls } from "./animate.js";
 
 const mousePos = { x: 0, y: 0 };
-const textElems = document.querySelectorAll("p,h2");
+const textElems = document.querySelectorAll("p,h2,section a");
 export const textNodeData: { visible: boolean; elem: HTMLElement }[] = Array(
     textElems.length,
 );
@@ -22,74 +22,88 @@ for (let i = 0; i < textElems.length; i++) {
     const frag = document.createDocumentFragment();
     for (let j = 0; j < textElem.childNodes.length; j++) {
         let node = textElem.childNodes[j]!;
+        let letters;
         if (node.nodeType === Node.TEXT_NODE) {
-            let letters = node.nodeValue;
-            if (!letters) continue;
-            const textFrag = document.createDocumentFragment();
-            let lastEmpty = true;
-            let wordFrag = document.createDocumentFragment();
-            for (let k = 0; k < letters.length; k++) {
-                let char = letters[k]!;
-                if (char.charCodeAt(0) <= " ".charCodeAt(0)) {
-                    if (lastEmpty) {
-                        continue;
-                    }
-                    lastEmpty = true;
-
-                    const wordSpan = document.createElement("span");
-                    wordSpan.classList.add("words");
-                    wordSpan.append(wordFrag);
-                    textFrag.append(wordSpan);
-                    textFrag.append(document.createTextNode(" "));
-                    wordFrag = document.createDocumentFragment();
-                    continue;
-                } else {
-                    lastEmpty = false;
-                }
-                const letterSpan = document.createElement("span");
-                letterSpan.classList.add("letters");
-                letterSpan.innerText = char;
-                wordFrag.append(letterSpan);
-            }
-            frag.append(textFrag);
+            letters = node.nodeValue;
         } else {
-            frag.append(node);
+            letters = node.textContent;
         }
+        if (!letters) continue;
+        const textFrag = document.createDocumentFragment();
+        let lastEmpty = true;
+        let wordFrag = document.createDocumentFragment();
+
+        const addToText = () => {
+            if (node.nodeType === Node.TEXT_NODE) {
+                const wordSpan = document.createElement("span");
+                wordSpan.classList.add("words");
+                wordSpan.append(wordFrag);
+                textFrag.append(wordSpan);
+            } else {
+                const elem = document.createElement(node.nodeName);
+                elem.append(wordFrag);
+                elem.classList.add("words");
+                textFrag.append(elem);
+            }
+        };
+        for (let k = 0; k < letters.length; k++) {
+            let char = letters[k]!;
+            if (char.charCodeAt(0) <= " ".charCodeAt(0)) {
+                if (lastEmpty) {
+                    continue;
+                }
+                lastEmpty = true;
+                addToText();
+                textFrag.append(document.createTextNode(" "));
+                wordFrag = document.createDocumentFragment();
+                continue;
+            } else {
+                lastEmpty = false;
+            }
+            const letterSpan = document.createElement("span");
+            letterSpan.classList.add("letters");
+            letterSpan.innerText = char;
+            wordFrag.append(letterSpan);
+        }
+        addToText();
+
+        frag.append(textFrag);
     }
+
     textElem.replaceChildren(frag);
     observer.observe(textElem);
 }
 
-const p0 = [0, -0.5, 1.5];
+const ps = [0, -0.5, 1.5];
+const lim = 100;
 
 const animateLetters = () => {
-    for (let p of textNodeData) {
-        if (!p.visible) {
+    for (let data of textNodeData) {
+        if (!data.visible) {
             continue;
         }
 
-        const words = p.elem.children;
+        const words = data.elem.children;
         for (let word of words) {
-            for (let l of word.children) {
-                const rect = l.getBoundingClientRect();
+            for (let letter of word.children) {
+                const rect = letter.getBoundingClientRect();
                 const left = rect.left + rect.width / 2;
                 const top = rect.top + rect.height / 2;
                 const dx = mousePos.x - left;
                 const dy = mousePos.y - top;
                 const d = Math.sqrt(Math.pow(dx, 2) + Math.pow(dy, 2));
-                if (d < 100) {
-                    const t = (100 - d) / 100;
+                if (d < lim) {
+                    const t = (lim - d) / lim;
                     const tsub = 1 - t;
-                    const v1 = [Math.pow(tsub, 2), 2 * tsub * t, t * t];
-                    let res = { x: 0, y: 0 };
+                    const vs = [Math.pow(tsub, 2), 2 * tsub * t, t * t];
+                    let res = 0;
                     for (let i = 0; i < 3; i++) {
-                        res.x += v1[i]! * p0[i]!;
-                        res.y += v1[i]! * p0[i]!;
+                        res += vs[i]! * ps[i]!;
                     }
-                    (l as HTMLElement).style.transform =
-                        `translate(${(dx / 100) * 1}px,${(1 - dy / 100) * 1}px) scale(${res.y * 0.5 + 1})`;
+                    (letter as HTMLElement).style.transform =
+                        `translate(${(1.5 * dx) / lim}px,${(1.5 * dy) / lim}px) scale(${res * 0.5 + 1})`;
                 } else {
-                    (l as HTMLElement).style.transform = "scale(1)";
+                    (letter as HTMLElement).style.transform = "scale(1)";
                 }
             }
         }
@@ -99,7 +113,6 @@ const animateLetters = () => {
 document.addEventListener("mousemove", (e) => {
     mousePos.x = e.clientX;
     mousePos.y = e.clientY;
-    console.log("move");
 });
 
 animateCalls.push(animateLetters);
