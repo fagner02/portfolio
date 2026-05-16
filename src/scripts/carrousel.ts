17842dd738cb087c97f6eb06a374478786f2ede9
Fagner Ferreira <fagnerrc12@gmail.com>
2026-05-16 20:12:50 -0300

    make carrousel cards selectable

--- src/scripts/carrousel.ts
+++ src/scripts/carrousel.ts
@@ -34,20 +34,67 @@ const observer = new IntersectionObserver((entries) => {
     }
 });
 
+let initialValues = [0, 0, 0, 0];
+let selectStart = 0;
+let selectDuration = 500;
+let clone: HTMLElement | null = null;
+let selecting: boolean = false;
+let deselecting: boolean = false;
+const cover = document.querySelector(".blackcover")! as HTMLElement;
+
+cover.addEventListener("click", () => {
+    selectStart = performance.now();
+    deselecting = true;
+});
 for (let i = 0; i < carrousels.length; i++) {
     const elems = carrousels[i]!.querySelectorAll<HTMLElement>(".card");
     carrousels[i]?.setAttribute("index", i.toString());
     observer.observe(carrousels[i]!);
     const cards: Card[] = Array(elems.length);
-    for (let i = 0; i < cards.length; i++) {
-        elems[i]!.style.willChange = "transform, opacity";
+    for (let j = 0; j < cards.length; j++) {
+        elems[j]!.style.willChange = "transform, opacity";
 
-        cards[i]! = {
-            elem: elems[i]!,
+        cards[j]! = {
+            elem: elems[j]!,
             y: Math.random(),
             x: Math.random(),
             width: 0,
         };
+
+        elems[j]!.addEventListener("click", () => {
+            console.log("clicked");
+            const elem = elems[j]!.cloneNode(true)! as HTMLElement;
+            const rect = elems[j]!.getBoundingClientRect();
+            const values = elems[j]!.style.transform.replace(/[A-Za-z()]/g, "")
+                .split(" ")
+                .map((x) => parseFloat(x));
+
+            clone = elem;
+            selecting = true;
+            console.log(values);
+            const viewport = window.visualViewport!;
+            const width = (100 * rect.width) / viewport.width;
+
+            console.log(width);
+            initialValues = [
+                values[2]!,
+                values[3]!,
+                values[4]!,
+                (100 * rect.y) / viewport.height,
+                (100 * rect.x) / viewport.width,
+                width,
+            ];
+            elem.style.zIndex = "1000000";
+            elem.style.position = "fixed";
+            elem.style.filter = "";
+            cover.style.opacity = "1";
+            cover.style.display = "flex";
+            (elem.firstElementChild as HTMLElement).style.width = "100%";
+
+            elem.id = "clone";
+            document.body.append(elem);
+            selectStart = performance.now();
+        });
     }
     carrouselsData[i] = {
         cards,
@@ -106,7 +153,54 @@ export const updateCarrousel = () => {
 updateCarrousel();
 
 const PI2 = Math.PI * 2;
+const targets = [0, 0, 0, 50, 50, 90];
+let values = Array(targets.length);
 const animateCarrousel = (now: number) => {
+    if (selecting && clone) {
+        let value = (now - selectStart) / selectDuration;
+        if (value > 1) value = 1;
+        for (let i = 0; i < 3; i++) {
+            values[i] =
+                value * (targets[i]! - initialValues[i]!) + initialValues[i]!;
+        }
+        let quarter = Math.pow(value * 1, 2);
+        if (quarter > 1) quarter = 1;
+        for (let i = 3; i < initialValues.length; i++) {
+            values[i] =
+                quarter * (targets[i]! - initialValues[i]!) + initialValues[i]!;
+        }
+
+        cover.style.opacity = `${value * 4}`;
+        clone.style.width = `${values[5]}%`;
+        clone.style.top = `${values[3]}%`;
+        clone.style.left = `${values[4]}%`;
+        clone.style.opacity = `${value * 4}`;
+        const pos = quarter * -50;
+        clone.style.transform =
+            `perspective(15cm) ` +
+            `translate(${pos}%,${pos}%) ` +
+            `rotateX(${values[0]!}deg) ` +
+            `rotateY(${values[1]!}deg) ` +
+            `rotateZ(${values[2]!}deg)`;
+        if (value === 1) {
+            selecting = false;
+        }
+    }
+    if (deselecting && clone) {
+        let value = Math.pow((now - selectStart) / selectDuration, 2);
+        if (value > 1) value = 1;
+        const subvalue = 1 - value;
+
+        cover.style.opacity = `${subvalue}`;
+        clone.style.opacity = `${subvalue}`;
+        clone.style.top = `${value * -150 + 50}%`;
+
+        if (value === 1) {
+            deselecting = false;
+            clone.remove();
+            cover.style.display = "none";
+        }
+    }
     for (let j = 0; j < carrouselsData.length; j++) {
         const carrousel = carrouselsData[j]!;
         if (!carrousel.visible) continue;
