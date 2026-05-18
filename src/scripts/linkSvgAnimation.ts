105ea2b0952ce16d02fa8d827982dad01f74648c
Fagner Ferreira <fagnerrc12@gmail.com>
2026-05-18 17:08:07 -0300

    add link animation to defs loaded callback

--- src/scripts/linkSvgAnimation.ts
+++ src/scripts/linkSvgAnimation.ts
@@ -1,76 +1,69 @@
 import { gsap } from "gsap";
 import { MorphSVGPlugin } from "gsap/MorphSVGPlugin";
+import { defsLoadedCallbacks } from "./loadDefs.js";
 
-const imageUrl = new URL("/src/assets/icons/defs.svg", import.meta.url).href;
+const callback = (svgElem: HTMLElement) => {
+    const stars = {
+        star: svgElem.querySelector("#star")?.getAttribute("d"),
+        star2: svgElem.querySelector("#star2")?.getAttribute("d"),
+    };
 
-fetch(imageUrl)
-    .then((response) => response.text())
-    .then(async (text) => {
-        const parser = new DOMParser();
-        const svgDoc = parser.parseFromString(text, "image/svg+xml");
-        const svgElem = svgDoc.firstElementChild!;
-        document.body.appendChild(svgElem);
-        const stars = {
-            star: (
-                svgElem.querySelector("#star") as SVGPathElement
-            ).getAttribute("d"),
-            star2: svgElem.querySelector("#star2")?.getAttribute("d"),
-        };
+    gsap.registerPlugin(MorphSVGPlugin);
 
-        gsap.registerPlugin(MorphSVGPlugin);
+    const links = document.querySelectorAll(".link");
 
-        const links = document.querySelectorAll(".link");
+    for (const link of links) {
+        const path = link.querySelector("svg>path") as SVGPathElement;
+        const className = path.classList.values().next().value;
+        if (!className) return;
 
-        for (const link of links) {
-            const path = link.querySelector("svg>path") as SVGPathElement;
-            const className = path.classList.values().next().value;
-            if (!className) return;
+        path.setAttribute("d", stars[className as keyof typeof stars]!);
 
-            path.setAttribute("d", stars[className as keyof typeof stars]!);
+        const tl = gsap.timeline({ paused: true, repeat: -1 });
 
-            const tl = gsap.timeline({ paused: true, repeat: -1 });
-
-            tl.to(path, { morphSVG: "#circle", duration: 0.5 })
-                .to(path, { morphSVG: "#collapsedh", duration: 0.5 })
-                .to(path, {
-                    morphSVG: `#${className}`,
-                    duration: 0.5,
-                    rotate: 180,
-                    transformOrigin: "center",
-                })
-                .to(path, { morphSVG: "#collapsed", duration: 0.5 })
-                .to(path, { morphSVG: "#peacock", duration: 0.5 })
-                .to(path, {
-                    morphSVG: "#circle",
-                    duration: 0.5,
-                    rotate: 360,
-                })
-                .to(path, { morphSVG: `#${className}`, duration: 0.5 })
-                .to(path, {
-                    morphSVG: "#flower",
-                    duration: 1,
-                    rotate: 180,
-                })
-                .to(path, { morphSVG: `#${className}`, duration: 0.5 })
-                .to(path, { morphSVG: "#violin", duration: 0.5 })
-                .to(path, {
-                    morphSVG: `#${className}`,
-                    duration: 0.5,
-                    rotate: 0,
-                });
-
-            (link as HTMLElement).addEventListener("mouseenter", () => {
-                tl.restart();
+        tl.to(path, { morphSVG: "#circle", duration: 0.5 })
+            .to(path, { morphSVG: "#collapsedh", duration: 0.5 })
+            .to(path, {
+                morphSVG: `#${className}`,
+                duration: 0.5,
+                rotate: 180,
+                transformOrigin: "center",
+            })
+            .to(path, { morphSVG: "#collapsed", duration: 0.5 })
+            .to(path, { morphSVG: "#peacock", duration: 0.5 })
+            .to(path, {
+                morphSVG: "#circle",
+                duration: 0.5,
+                rotate: 360,
+            })
+            .to(path, { morphSVG: `#${className}`, duration: 0.5 })
+            .to(path, {
+                morphSVG: "#flower",
+                duration: 1,
+                rotate: 180,
+            })
+            .to(path, { morphSVG: `#${className}`, duration: 0.5 })
+            .to(path, { morphSVG: "#violin", duration: 0.5 })
+            .to(path, {
+                morphSVG: `#${className}`,
+                duration: 0.5,
+                rotate: 0,
             });
 
-            link.addEventListener("mouseleave", () => {
-                tl.kill();
-                gsap.to(path, {
-                    duration: 0.5,
-                    morphSVG: `#${className}`,
-                    rotate: 0,
-                    overwrite: true,
-                });
+        (link as HTMLElement).addEventListener("mouseenter", () => {
+            tl.restart();
+        });
+
+        link.addEventListener("mouseleave", () => {
+            tl.kill();
+            gsap.to(path, {
+                duration: 0.5,
+                morphSVG: `#${className}`,
+                rotate: 0,
+                overwrite: true,
             });
-        }
-    });
+        });
+    }
+};
+
+defsLoadedCallbacks.push(callback);
