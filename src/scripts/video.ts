75a335ab7a64608410ed7008f51658513574eb8a
Fagner Ferreira <fagnerrc12@gmail.com>
2026-05-22 00:14:53 -0300

    set video attributes

--- src/scripts/video.ts
+++ src/scripts/video.ts
@@ -1,6 +1,6 @@
 const videos = document.querySelectorAll("video");
 for (let i = 0; i < videos.length; i++) {
-    videos[i]?.setAttribute("autoplay", "true");
-    videos[i]?.setAttribute("muted", "true");
-    videos[i]?.setAttribute("loop", "true");
+    videos[i]!.muted = true;
+    videos[i]!.autoplay = true;
+    videos[i]!.loop = true;
 }
