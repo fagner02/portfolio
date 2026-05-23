61146f4f40b4ae83fe2edf4072a85fb181d84c91
Fagner Ferreira <fagnerrc12@gmail.com>
2026-05-23 12:53:46 -0300

    add info to vansi section

--- src/scripts/video.ts
+++ src/scripts/video.ts
@@ -1,6 +1,17 @@
+const observer = new IntersectionObserver((entries) => {
+    for (const e of entries) {
+        if (e.isIntersecting) {
+            (e.target as HTMLVideoElement).play();
+        } else {
+            (e.target as HTMLVideoElement).pause();
+        }
+    }
+});
+
 const videos = document.querySelectorAll("video");
 for (let i = 0; i < videos.length; i++) {
     videos[i]!.muted = true;
     videos[i]!.autoplay = true;
     videos[i]!.loop = true;
+    observer.observe(videos[i]!);
 }
