d4e832d1ca3b7b7ce97ad44ef770292a776f5373
Fagner Ferreira <fagnerrc12@gmail.com>
2026-05-24 11:33:05 -0300

    update model loader section

--- src/scripts/video.ts
+++ src/scripts/video.ts
@@ -1,17 +1,27 @@
 const observer = new IntersectionObserver((entries) => {
     for (const e of entries) {
-        if (e.isIntersecting) {
-            (e.target as HTMLVideoElement).play();
-        } else {
-            (e.target as HTMLVideoElement).pause();
+        if ((e.target as HTMLVideoElement).readyState > 2) {
+            try {
+                if (e.isIntersecting) {
+                    (e.target as HTMLVideoElement).play();
+                } else {
+                    (e.target as HTMLVideoElement).pause();
+                }
+            } catch (e) {}
         }
     }
 });
 
 const videos = document.querySelectorAll("video");
+const onLoad = (e: Event) => {
+    const video = e.target as HTMLVideoElement;
+    video.removeEventListener("loadeddata", onLoad);
+    video.muted = true;
+    video.autoplay = true;
+    video.loop = true;
+    observer.observe(video);
+};
+
 for (let i = 0; i < videos.length; i++) {
-    videos[i]!.muted = true;
-    videos[i]!.autoplay = true;
-    videos[i]!.loop = true;
-    observer.observe(videos[i]!);
+    videos[i]!.addEventListener("loadeddata", onLoad);
 }
