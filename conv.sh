1c425aa552a500d5998f9d1189bae0724971bba5
Fagner Ferreira <fagnerrc12@gmail.com>
2026-05-25 00:00:42 -0300

    raise npi images quality

--- conv.sh
+++ conv.sh
@@ -1,12 +1,12 @@
-# file="/pathfinding/pathf2"
-# ffmpeg -i ./src/assets/${file}.mp4 \
-# -vf "fps=15,setpts=0.5*PTS,scale=480:-1:flags=lanczos" \
-# -q:v 80 -loop 0 \
-# ./src/assets/${file}.webp
-
-file="/pathfinding/pfind3"
-ffmpeg -i ./src/assets/${file}.png -vf "scale=1050:-1" \
+file="/pathfinding/pathf2"
+ffmpeg -i ./src/assets/${file}.mp4 \
+-vf "fps=15,setpts=0.5*PTS,scale=800:-1:flags=lanczos" \
+-q:v 80 -loop 0 \
 ./src/assets/${file}.webp
+
+# file="/pathfinding/pfind3"
+# ffmpeg -i ./src/assets/${file}.png -vf "scale=1050:-1" \
+# ./src/assets/${file}.webp
 
 
 
