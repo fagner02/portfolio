a19a23be4db8be6e794c74ff39a2ac60273e09b4
Fagner Ferreira <fagnerrc12@gmail.com>
2026-05-24 23:11:24 -0300

    convert pngs to webp

--- conv.sh
+++ conv.sh
@@ -1,7 +1,11 @@
-file="/vansi/vansiall"
-ffmpeg -i ./src/assets/${file}.mp4 \
--vf "fps=10,setpts=0.5*PTS,scale=600:-1:flags=lanczos" \
--q:v 80 -loop 0 \
+# file="/vansi/vansiall"
+# ffmpeg -i ./src/assets/${file}.mp4 \
+# -vf "fps=10,setpts=0.5*PTS,scale=600:-1:flags=lanczos" \
+# -q:v 80 -loop 0 \
+# ./src/assets/${file}.webp
+
+file="/vansi/vansi4"
+ffmpeg -i ./src/assets/${file}.png \
 ./src/assets/${file}.webp
 
 
