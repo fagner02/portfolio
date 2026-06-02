4c1539c780529d166bf22b96bbd0473d7bce742a
Fagner Ferreira <fagnerrc12@gmail.com>
2026-06-02 18:59:21 -0300

    add expenses section info

--- conv.sh
+++ conv.sh
@@ -1,14 +1,14 @@
-# file="/bubbles/play3"
-# ffmpeg -i ./src/assets/${file}.mp4 \
+file="/expenses/input"
+ffmpeg -i ./src/assets/${file}.mp4 \
+-vf "fps=15,setpts=1*PTS,scale=600:-1:flags=lanczos" \
+-q:v 80 -loop 0 \
+./src/assets/${file}.webp
 # -ss 00:00:00 \
 # -t 3 \
-# -vf "fps=18,setpts=0.6*PTS,scale=600:-1:flags=lanczos" \
-# -q:v 80 -loop 0 \
-# ./src/assets/${file}.webp
 
-file="/expenses/register"
-ffmpeg -i ./src/assets/${file}.png -vf "scale=1080:-1" \
-./src/assets/${file}.webp
+# file="/expenses/welcome"
+# ffmpeg -i ./src/assets/${file}.png -vf "scale=1080:-1" \
+# ./src/assets/${file}.webp
 
 
 
