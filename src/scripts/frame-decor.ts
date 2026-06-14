d7b7b141e8bc4222f7381992c926b618d0e67ea5
Fagner Ferreira <fagnerrc12@gmail.com>
2026-06-14 15:57:40 -0300

    fix frame decor blinking

--- src/scripts/frame-decor.ts
+++ src/scripts/frame-decor.ts
@@ -1,11 +1,11 @@
 const observer = new ResizeObserver((entries) => {
     for (const e of entries) {
-        if (e.contentRect.width < 40) {
+        if (e.contentRect.width < 10) {
             (e.target as HTMLElement).style.contentVisibility = "hidden";
-            (e.target as HTMLElement).style.border = "none";
+            (e.target as HTMLElement).style.opacity = "0";
         } else {
             (e.target as HTMLElement).style.contentVisibility = "visible";
-            (e.target as HTMLElement).style.border = "1px solid";
+            (e.target as HTMLElement).style.opacity = "1";
         }
     }
 });
